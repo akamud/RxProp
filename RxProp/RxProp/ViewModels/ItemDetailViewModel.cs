00e9d49536ba289df1a974a39741fb30d2753b37
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2019-08-04 13:42:10 -0300

    RxUI.Fody

--- RxProp/RxProp/ViewModels/ItemDetailViewModel.cs
+++ RxProp/RxProp/ViewModels/ItemDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using RxProp.Models;
 using System;
 using System.Diagnostics;
@@ -11,32 +12,22 @@ namespace RxProp.ViewModels
     {
         public Item Item { get; set; }
 
-        private string cep;
+        [Reactive]
+        public string CEP { get; set; }
 
-        public string CEP
-        {
-            get { return cep; }
-            set { this.RaiseAndSetIfChanged(ref cep, value); }
-        }
-
-        ObservableAsPropertyHelper<string> cepBusca;
-
-        public string CEPBusca
-        {
-            get { return cepBusca.Value; }
-        }
+        public string CEPBusca { [ObservableAsProperty] get; }
 
         public ItemDetailViewModel(Item item = null)
         {
             Item = item;
 
             CEP = "";
-            cepBusca = this.WhenAnyValue(x => x.CEP)
+            this.WhenAnyValue(x => x.CEP)
                 .Throttle(TimeSpan.FromMilliseconds(500))
                 .Where(x => !string.IsNullOrWhiteSpace(x) && x.Length == 9)
                 .DistinctUntilChanged()
                 .Select(x => x.Replace("-", "").Trim())
-                .ToProperty(this, e => e.CEPBusca);
+                .ToPropertyEx(this, e => e.CEPBusca);
 
             var result = this.WhenAnyValue(x => x.CEPBusca)
                 .Select(Write)
